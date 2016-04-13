0b1f23df2872e46cebbd37f124407cecd18e7921
Mario Seattle <marioseattle@live.com>
2016-04-13 09:53:06 -0700

    Adds wordpress like header/footer files to our app.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,15 +1,16 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Welcome extends CI_Controller {
 	public function index()
 	{
-        //associative array of data to pass to view
-        $data['title'] = 'Mario Seattle';
-        $data['page_id'] = 'Mario_Seattle';
-        $data['guts'] = 'Hands on CodeIgniter SP16';
+		//associative array of data to pass to view
+        $data['title'] = "My Unique Title Tag, now inside header!";
+        $data['page_id'] = "My Clever Page ID, in header!";
         
-        //data is pass as second parameter in the view creation
-		$this->load->view('welcome_message', $data);
+        $data['guts'] = '<p>I am still the creamy filling inside!</p>';
+        
+        $this->load->view('templates/header',$data);
+        $this->load->view('welcome_message',$data);
+        $this->load->view('templates/footer',$data);
 	}
 }
