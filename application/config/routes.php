078c2cf9ef9188fb167950b68c2370e390ee73b0
Mario Seattle <marioseattle@live.com>
2016-04-20 13:18:39 -0700

    News/Create file. It needs to be fix

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,9 +50,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 //routes for the new controler
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+
+
--- application/models/News_model.php
+++ application/models/News_model.php
@@ -17,5 +17,37 @@ class News_model extends CI_Model {
             $query = $this->db->get_where('sp16_news', array('slug' => $slug));
             return $query->row_array();
     }
+    
+    public function set_news()
+    {
+        $this->load->helper('url');
+
+        $slug = url_title($this->input->post('title'), 'dash', TRUE);
+
+        $data = array(
+            'title' => $this->input->post('title'),
+            'slug' => $slug,
+            'text' => $this->input->post('text')
+            );
+        
+        if($this->db->insert('sp16_news', $data))
+        {//good data show reccords
+            $id = $this->db->insert_id();
+            die;
+            
+            
+            $query = $this->db->get_where('sp16_news', array('id' => $id));
+            return $query->row_array();
+            
+            
+        }else{//Bad data ?? Feedback
+            echo "Ohh Ohh";
+            die;
+        }
+
+        //return $this->db->insert('sp16_news', $data);
+    }
+       
+    
 }
 
