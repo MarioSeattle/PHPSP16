1ece7107a09a5103ee8f8a28cbced01055c2736f
Mario Seattle <marioseattle@live.com>
2016-04-23 14:01:53 -0700

    Added some text to play with logic MVC

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -7,7 +7,7 @@ class Welcome extends CI_Controller {
         $data['title'] = "My Unique Title Tag, now inside header!";
         $data['page_id'] = "My Clever Page ID, in header!";
         
-        $data['guts'] = '<p>I am still the creamy filling inside!</p>';
+        $data['guts'] = '<p>I am still the creamy filling inside! Making some changes for logical exercise</p>';
         
         $this->load->view('templates/header',$data);
         $this->load->view('welcome_message',$data);
